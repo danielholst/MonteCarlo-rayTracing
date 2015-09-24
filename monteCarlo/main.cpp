9b185ccb3fbab3768c53b0b1fcadfcc1f070e2d7
DanielHolst <danielholst2@gmail.com>
2015-09-24 13:45:14 +0200

    Initial Commit

---
+++ monteCarlo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  monteCarlo
+//
+//  Created by Daniel Holst on 2015-09-24.
+//  Copyright (c) 2015 Daniel Holst. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
